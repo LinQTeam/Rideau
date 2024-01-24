03b62352a87e6eeff99d96b11f59941b3d07b644
eric <imeric@>
2024-01-24 23:16:35 +0900

    fix the endless loop issue

--- Rideau/Core/ScrollController.swift
+++ Rideau/Core/ScrollController.swift
@@ -28,6 +28,7 @@ final class ScrollController {
 
   private var scrollObserver: NSKeyValueObservation!
   private(set) var isLocking: Bool = false
+  private var isHandlingEvent: Bool = false
   private var previousValue: CGPoint?
   let scrollView: UIScrollView
 
@@ -81,20 +82,39 @@ final class ScrollController {
   private func handleScrollViewEvent(scrollView: UIScrollView, change: NSKeyValueObservedChange<CGPoint>) {
 
     // For debugging
+    if isHandlingEvent { return }
+    isHandlingEvent = true
 
-    guard let oldValue = change.oldValue else { return }
-
-    guard isLocking else {
+    guard let oldValue = change.oldValue else {
+      isHandlingEvent = false
       return
     }
 
-    guard scrollView.contentOffset != oldValue else { return }
+    if oldValue.y != scrollView.contentOffset.y {
+      isHandlingEvent = false
+      return
+    }
+    
+    guard isLocking else {
+      isHandlingEvent = false
+      return
+    }
 
-    guard oldValue != previousValue else { return }
+    guard scrollView.contentOffset != oldValue else {
+      isHandlingEvent = false
+      return
+    }
+
+    guard oldValue != previousValue else {
+      isHandlingEvent = false
+      return
+    }
 
     previousValue = scrollView.contentOffset
 
     scrollView.setContentOffset(oldValue, animated: false)
+    
+    isHandlingEvent = false
   }
 
 }
